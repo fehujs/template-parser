d1c5811bc0f859a239553ed3caacfd0a63653334
Pascal LLEONART <pascal.lleonart@laposte.net>
2024-12-09 16:51:17 +0100

    setup project

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { TemplateNotFound, VariableMissingInContext, WrongTypeVariable } from "./errors"
+import { readFileSync } from "fs"
+import { parse } from "./handlers"
+import { type Context } from "./types"
+
+
+function render(path: string, context?: Context) {
+    let template = "<pre>[template-parser] default html template</pre>"
+    try {
+        template = readFileSync(path, { encoding: "utf8" })
+    } catch (e) {
+        new TemplateNotFound(path)
+    }
+
+    if (context) return parse(template, context)
+
+    return parse(template, {})
+}
+
+type TemplateParserConfig = {}
+
+export {
+    Context,
+    render,
+    parse,
+    TemplateNotFound,
+    TemplateParserConfig,
+    VariableMissingInContext,
+    WrongTypeVariable
+}
---
+++ src/types.ts
@@ -0,0 +1,2 @@
+
+export type Context = Record<string, string | string[] | number | number[] | boolean | boolean[]>
